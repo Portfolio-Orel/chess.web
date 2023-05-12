1a6e5ec26b020a827ee42d91c4b5613880b3e400
Orelzman <orelsmail@gmail.com>
2023-05-12 19:30:11 +0300

    - Build fix

--- src/pages/middleware.js
+++ src/pages/middleware.js
@@ -1,15 +1,18 @@
-// middleware.ts
 import { NextResponse } from "next/server";
-import Cookies from "js-cookie";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request) {
   const requestHeaders = new Headers(request.headers);
-  const token = Cookies.get("token");
-  const userId = Cookies.get("userid");
 
-  requestHeaders.set("userid", userId);
-  requestHeaders.set("Authorization", token);
+  // Check if running in the browser environment
+  if (typeof window !== "undefined") {
+    const Cookies = require("js-cookie");
+    const token = Cookies.get("token");
+    const userId = Cookies.get("userid");
+
+    requestHeaders.set("userid", userId);
+    requestHeaders.set("Authorization", token);
+  }
+
   return NextResponse.next({
     request: {
       headers: requestHeaders,
